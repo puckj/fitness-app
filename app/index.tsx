445bc6f9a28fe955713e3853cf65879c58ec9b7b
Pongsapuck_Dev <pongsapuck.j@gmail.com>
2024-02-08 10:15:05 +0700

    welcome screen ui

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,55 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, StatusBar } from "react-native";
 import React from "react";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
+import { LinearGradient } from "expo-linear-gradient";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const Index = () => {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>First screen</Text>
+    <View className="flex-1 flex justify-end">
+      <StatusBar barStyle={"light-content"} />
+      <Image
+        className="h-full w-full absolute"
+        source={require("../assets/images/welcome.png")}
+      />
+      <LinearGradient
+        colors={["transparent", "#18181b"]}
+        style={{ width: wp(100), height: hp(70) }}
+        start={{ x: 0.5, y: 0 }}
+        end={{ x: 0.5, y: 0.8 }}
+        className="flex justify-end pb-12 space-y-8"
+      >
+        <View className="flex items-center">
+          <Text
+            style={{ fontSize: hp(5) }}
+            className="text-white font-bold tracking-wide"
+          >
+            Best <Text className="text-rose-500">Workouts</Text>
+          </Text>
+          <Text
+            style={{ fontSize: hp(5) }}
+            className="text-white font-bold tracking-wide"
+          >
+            For you
+          </Text>
+        </View>
+        <View>
+          <TouchableOpacity
+            style={{ height: hp(7), width: wp(80) }}
+            className="bg-rose-500 flex items-center justify-center mx-auto rounded-full border-[1px] border-neutral-200"
+          >
+            <Text
+              style={{ fontSize: hp(3) }}
+              className="text-white font-bold tracking-widest"
+            >
+              Get Started
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </LinearGradient>
     </View>
   );
 };
